946ab210550b2e6d6d1b15cfd5c18ec1af4bffb8
Elia Giolli <eliagiolli22@gmail.com>
2026-09-09 16:27:38 +0200

    about page ready

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -1,2 +1,8 @@
 export const BLOG_NAME = "Imperi e Rivoluzioni"
 export const LOGO_ALT_TEXT = "Il logo del blog"
+
+// Socials
+
+export const INSTAGRAM_URL = 'https://www.instagram.com/imperierivoluzioni/';
+export const SUBSTACK_URL = "https://imperierivoluzioni.substack.com/?r=92ksfy&utm_campaign=pub-share-checklist";
+export const MAIL = 'rivoluzionieimperi@gmail.com';
